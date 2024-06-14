a58ab115685db1f080cbc36820d6a5c8d90149e3
Arpan Patel <arpan.p.se@gmail.com>
2024-06-14 22:19:26 +0000

    added image function

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -27,4 +27,35 @@ class ActorsController < ApplicationController
     redirect_to("/actors")
 
   end
+
+
+  def update
+    act_id = params.fetch("an_id")
+
+    matching_records = Actor.where({ :id => act_id })
+
+    the_actor = matching_records.at(0)
+
+    the_actor.name = params.fetch("query_name")
+    the_actor.dob = params.fetch("query_dob")
+    the_actor.bio = params.fetch("query_bio")
+    the_actor.image = params.fetch("query_image")
+    the_actor.save
+
+    redirect_to("/actors/#{the_actor.id}")
+  end
+  
+    
+    def destroy
+      the_id = params.fetch("an_id")
+  
+      matching_records = Actor.where({ :id => the_id })
+  
+      the_actor = matching_records.at(0)
+  
+      the_actor.destroy
+  
+      redirect_to("/actors")
+    end
+
 end
